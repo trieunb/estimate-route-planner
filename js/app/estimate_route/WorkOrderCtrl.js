fe7d0b008d3baae2671c858289bbe532ad22ff94
luanht <htluan2605@gmail.com>
2015-09-25 16:44:01 +0700

    done print work order

--- js/app/estimate_route/WorkOrderCtrl.js
+++ js/app/estimate_route/WorkOrderCtrl.js
@@ -7,15 +7,33 @@ angular
             '$rootScope',
             '$routeParams',
             'estimateRouteFactory',
+            'estimateFactory',
             WorkOrderCtrl
         ]
     );
 
-function WorkOrderCtrl($scope, $rootScope, $routeParams, estimateRouteFactory) {
+function WorkOrderCtrl(
+        $scope,
+        $rootScope,
+        $routeParams,
+        estimateRouteFactory,
+        estimateFactory) {
     $scope.setPageTitle('Create Work Order');
-    $scope.route = {};
-    estimateRouteFactory.get($routeParams.id).
+    $scope.route = {
+        assignedEstimates: []
+    };
+    var routeId = $routeParams.id;
+    estimateRouteFactory.get(routeId).
         success(function(data) {
             $scope.route = data;
+
+            estimateFactory.listAssigedToRoute(routeId).
+                success(function(responseData) {
+                    $scope.route.assignedEstimates = responseData;
+                });
         });
+
+    $scope.print = function() {
+        window.print();
+    };
 }
