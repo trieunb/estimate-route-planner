4fdeb94c2248f421f40c021d281f20270e43f8c6
luanht <htluan2605@gmail.com>
2015-10-02 10:45:25 +0700

    clean

--- estimate-route-planner.php
+++ estimate-route-planner.php
@@ -12,7 +12,6 @@ define('ERP_PLUGIN_DIR', plugin_dir_path(__FILE__)); // Physical root path of pl
 define('ROOT_MENU_SLUG', 'erpp');
 define('ERP_CONFIG_PAGE_SLUG', 'estimate-route-planner-config');
 define('ERPP_NAVIGATION_CLASS', 'estimate-route-planner-menu');
-define('ERP_SESSION_SAVE_PATH', '/tmp');
 define('ERP_PLUGIN_NAME', 'ER Planner Pro');
 
 require_once(ERP_PLUGIN_DIR . '/config/plugin.php');
