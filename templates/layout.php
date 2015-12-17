97655ff7ecb37c972c12771793ad90448c37b839
luanht <htluan2605@gmail.com>
2015-12-17 11:10:00 +0700

    hide refresh button on print

--- templates/layout.php
+++ templates/layout.php
@@ -2,7 +2,7 @@
     <h1 id="erp-header"><span ng-bind="pageTitle"></span></h1>
     <div class="erp-tool-bar text-right">
         <button type="button"
-            class="btn btn-default btn-sm ng-cloak btn-refresh-page"
+            class="btn btn-default btn-sm ng-cloak btn-refresh-page hide-print"
             ng-disabled="isBusy"
             ng-click="refreshPage()">
             <span class="glyphicon glyphicon-refresh"></span></button>
