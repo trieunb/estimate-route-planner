6898668bca903519052d835afc9361b9a17b2dff
trieunb <trieunb08@gmail.com>
2016-03-03 17:48:59 +0700

    fix code format date

--- js/app/filters/datetimeToDate.js
+++ js/app/filters/datetimeToDate.js
@@ -1,7 +1,6 @@
 angular.module('Erp')
     .filter('datetimeToDate', function($filter) {
       return function(datetime, format) {
-        var date = Date.parse(datetime)
-        return $filter('date')(date, format);
+        return $filter('date')(Date.parse(datetime), format);
     };
 });
