41b0308cf75eeb589154055560f9bfb10992d192
trieunb <trieunb08@gmail.com>
2016-03-04 13:44:43 +0700

    clear code

--- js/app/directives/datepickerAutoOpen.js
+++ js/app/directives/datepickerAutoOpen.js
@@ -1,15 +1,15 @@
- angular
-    .module('Erp')
-    .directive("datepickerAutoOpen", ["$parse", "$timeout", function($parse, $timeout) {
-        return {
-          link: function(scope, element, attrs) {
-              var elmScope = element.isolateScope();
-              element.on("click", function() {
-                $timeout(function() {
-                  $parse("isOpen").assign(elmScope, "true");
-                  elmScope.$apply();
-                })
+angular
+  .module('Erp')
+  .directive("datepickerAutoOpen", ["$parse", "$timeout", function($parse, $timeout) {
+      return {
+        link: function(scope, element, attrs) {
+            var elmScope = element.isolateScope();
+            element.on("click", function() {
+              $timeout(function() {
+                $parse("isOpen").assign(elmScope, "true");
+                elmScope.$apply();
               });
-          }
-        };
-    }]);
+            });
+        }
+      };
+  }]);
