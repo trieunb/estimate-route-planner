63a821c76328c0506b35b7ec99041eeb5b705fe8
trieunb <trieunb08@gmail.com>
2016-03-04 09:22:03 +0700

    fix missing datetime null

--- js/app/filters/datetimeToDate.js
+++ js/app/filters/datetimeToDate.js
@@ -1,6 +1,8 @@
 angular.module('Erp')
     .filter('datetimeToDate', function($filter) {
       return function(datetime, format) {
-        return $filter('date')(Date.parse(datetime), format);
+        if (datetime) {
+            return $filter('date')(Date.parse(datetime), format);
+        }
     };
 });
