7b5b118043d66d2fa506b7933dfc44692ce10586
luanht <htluan2605@gmail.com>
2016-03-12 09:44:03 +0700

    more clean code

--- classes/models/SyncHistoryModel.php
+++ classes/models/SyncHistoryModel.php
@@ -1,21 +1,8 @@
 <?php
 class SyncHistoryModel extends BaseModel {
 
-    const MAX_WAITING_TIME_FOR_LAST_SYNC = 600; // in seconds == 10 minutes
-
     public function getTableName() {
         return 'sync_histories';
-    }
-
-    public static function isSoLongNotSync() {
-        $lastSync = ORM::forTable('sync_histories')
-            ->orderByDesc('start_at')
-            ->findOne();
-        if (!$lastSync) {
-            return true;
-        } else {
-            return (time() - strtotime($lastSync->start_at)) >= self::MAX_WAITING_TIME_FOR_LAST_SYNC;
-        }
     }
 }
 ?>
