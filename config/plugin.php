e7b4d2c2de9b1f420170461718751389c2395980
luanht <htluan2605@gmail.com>
2016-03-21 17:12:57 +0700

    change to support local configuration

--- config/plugin.php
+++ config/plugin.php
@@ -12,26 +12,31 @@ function erp_define_if_not($name, $val) {
     }
 }
 
-/* Debugging flag */
-erp_define_if_not('ERP_DEBUG', false);
+/* Check override configuration in local */
+if (file_exists(__DIR__ . '/plugin.local.php')) {
+    include __DIR__ . '/plugin.local.php';
+} else {
+    /* Debugging flag */
+    erp_define_if_not('ERP_DEBUG', false);
 
-/* Quickbooks sanbox mode flag */
-erp_define_if_not('QB_SANDBOX_MODE', ERP_DEBUG);
+    /* Quickbooks sandbox mode flag */
+    erp_define_if_not('QB_SANDBOX_MODE', false);
 
-/* Timezone */
-erp_define_if_not('ERP_TIMEZONE', 'UTC');
+    /* Plugin timezone */
+    erp_define_if_not('ERP_TIMEZONE', 'UTC');
 
+    /* Memcached server */
+    erp_define_if_not('ERP_MEMCACHED_HOST', '127.0.0.1');
+    erp_define_if_not('ERP_MEMCACHED_PORT', 11211);
+    erp_define_if_not('ERP_CACHE_PREFIX', 'erpp');
+}
+
+/* Set timezone for all php date time functions */
 if (date_default_timezone_get() != ERP_TIMEZONE) {
     date_default_timezone_set(ERP_TIMEZONE);
 }
 
-erp_define_if_not('ERP_MEMCACHED_HOST', '127.0.0.1');
-
-erp_define_if_not('ERP_MEMCACHED_PORT', 11211);
-
-erp_define_if_not('ERP_CACHE_PREFIX', 'erpp');
-
-/* Max excution time */
+/* Max execution time */
 set_time_limit(600); // 10 mins
 
 ?>
